1557418ddc4865536ece0a4b145d9a73d01b90c8
tmmcmasters <tmmcmasters@gmail.com>
2023-03-04 15:17:08 -0500

    Creating General Outline of Room Movement

--- Src/Map/House.Move.cs
+++ Src/Map/House.Move.cs
@@ -2,12 +2,16 @@
 
 public partial class House
 {
+    public Room CurrentRoom { get; set; } //Represents the current room the player is in so we can access it anywhere in our game. 
     public void GoToRoom (int index)
     {
+        if (CurrentRoom != null)
+            CurrentRoom.Visisted = true;
 
+        CurrentRoom = Rooms[index];
     }
     public void GoToStartingRoom()
     {
-    
+        GoToRoom(_rnd.Next(0, Rooms.Length));
     }
 }
--- Src/Map/House.cs
+++ Src/Map/House.cs
@@ -6,6 +6,9 @@ public partial class House //Tells the compiler to look at any other files in ou
 
     private readonly Random _rnd = new(1234); //read only tells the compiler to automaticall set the value of the field when compiling and it cant be changed at runtime. 
 
+    public int Width { get; set; }
+    public int Height { get; set; } //Width and height defineds the house as a grid. 
+    public Room[] Rooms { get; private set; } //Keeping the rooms inside a one dimensional array
     public House (Player player)
     {
         Player = player;
