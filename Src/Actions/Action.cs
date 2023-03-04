4e00df82fa1ce929b4f0f884b2f3c8ce46fd66fb
tmmcmasters <tmmcmasters@gmail.com>
2023-03-04 15:30:17 -0500

    Single Actions Instance that we can use anywhere inside of our game.

---
+++ Src/Actions/Action.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPADVENTURE;
+
+public abstract class Action
+{
+    //Each action is this game is going to have a unique identifier called Name. Creating a property for Name using Virtual. Virtual tells the compiler that other classes that exten the action class are able to override the getter and setter of the virtual property. 
+    public virtual string Name => ""; //WE use a => to automaticall generate a getter for us. The name will be set to base empty string on the base action class. 
+
+    public virtual void Execute(string[] args)
+    {
+        throw new Exception("Nothing to execute");
+    } 
+}
---
+++ Src/Actions/Actions.cs
@@ -0,0 +1,26 @@
+﻿namespace OOPADVENTURE;
+
+
+//This is all the logic that will allow us to create a single actions instance that we can use anywhere inside of our game. 
+public sealed class Actions
+{
+    private static Actions _instance;
+
+    public static Actions Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new Actions();
+
+            return _instance;
+        }
+    }
+
+    private readonly Dictionary<string, Action> _registeredActions = new();
+
+    private Actions ()
+    {
+
+    }
+}
